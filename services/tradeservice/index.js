d12487fa3b083aba374872c03d832ea9eb7447f3
Boopesh <tboopeshkumar@gmail.com>
2018-10-01 00:26:54 +0530

    API gateway initial implementation.

--- services/tradeservice/index.js
+++ services/tradeservice/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const axios = require('axios').default;
-const serviceRegistryURL = `http://127.0.0.1:4000/service`;
+const serviceRegistryURL = `http://localhost:3000/service`;
 const port = 3010;
 const dataStore = require('./datastore/datastore');
 
